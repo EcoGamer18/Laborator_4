c5390a28e014738b1e72cc58f8e76ea6eb82a528
EcoGamer18 <gabymeow07@gmail.com>
2021-04-06 07:43:21 +0300

    noted stacks

--- Laborator_4/UI.cpp
+++ Laborator_4/UI.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 
 void print_menu() {
-	std::cout << "\n1.Adaugati un numar complex.\n";
+	std::cout << "\nStacks baby\n1.Adaugati un numar complex.\n";
 	std::cout << "2.Afisare numere complexe salvate.\n";
 	std::cout << "3.Determinarea numarului cu cel mai mare modul.\n";
 	std::cout << "4.Identificarea celei mai lungi secvente de entitati egale.\n";
